93003a1a34ffdba2db208867a0347b912064366e
Neerajv2000 <147934600+Neerajv2000@users.noreply.github.com>
2023-10-31 23:37:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-kw3rs6
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/Neerajv2000/web-platform-kw3rs6)
---
+++ script.js
@@ -0,0 +1,29 @@
+// let a = prompt ("enter input");
+
+// if(a=="html" || a=="HTML"  || a=="Html"){console.log(" we are learnig " + a);}
+
+// else if(a=="css" || a=="CSS"  || a=="Css"){console.log(" we are learnig " + a);}
+
+// else if(a=="java script" || a=="JAVA SCRIPT"  || a=="Java script"){console.log(" we are learnig " + a);}
+
+// else if(a=="python" || a=="PYTHON"  || a=="Python"){console.log(" we are learnig " + a);}
+
+// else{console.log("check your value ");}
+
+
+let a = promp ("edho okati rayi ra babu");
+
+switch(a){
+  case 'na gunde':
+  console.log("joke");
+  break;
+  case 'ne gunde':
+  console.log("angry");
+  break;
+  case 'pakana gunde':
+  console.log("mad");
+  break;
+  case 'venaka gunde':
+  console.log("peace");
+  break;
+}
